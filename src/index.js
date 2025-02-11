bc0d4ee5019dd9ea69f10f8599b180d1e60b991e
M-azam-irshad <196023029+M-azam-irshad@users.noreply.github.com>
2025-02-11 19:11:29 +0500

    Added validation functionality

--- src/index.js
+++ src/index.js
@@ -1,34 +1,44 @@
-
+let usedCell = [];
 
 class Ship {
   #length;
   #coords;
   #hits = 0;
 
-  constructor(size,cordinates) {
+  constructor(size, coordinates) {
+    if (!Array.isArray(coordinates) || coordinates.length !== size) {
+      throw new Error("Coordinates must be an array of length equal to size");
+    }
+    
     this.#length = size;
-    this.#coords = [];
+    this.#coords = coordinates;
     this.#hits = 0;
-    this.#coords = cordinates;
   }
-
 
   hit() {
     this.#hits++;
-    console.log("It worked");
-    
+    console.log(`Ship hit! Total hits: ${this.#hits}`);
   }
 
   sunk() {
-    let sunk = this.#hits >= this.#length;
-    return sunk;
-    // Method implementation
+    return this.#hits >= this.#length;
   }
 
   match(input) {
-    return this.#coords.some((arr) => {
-      return JSON.stringify(arr) === JSON.stringify(input);
-    });
+    
+    if(usedCell.some(([x,y]) => x === input[0] && y === input[1]))
+    {
+      console.log("Alread selected");
+      
+      return false;
+    }
+    else
+    {
+      usedCell.push(input)
+      console.log(usedCell);
+      
+      return this.#coords.some(([x, y]) => x === input[0] && y === input[1]);
+    }
   }
 }
 
@@ -45,31 +55,62 @@ class Computer {
 }
 
 class Board {
-  newArr = Array.from({ length: 100 }, (_, index) => {
-    const i = Math.floor(index / 10);
-    const j = index % 10;
-    return [i, j];
-  });
+  constructor() {
+    this.grid = Array.from({ length: 10 }, (_, i) =>
+      Array.from({ length: 10 }, (_, j) => [i, j])
+    );
+  }
 }
 
 let B1 = new Board();
 let B2 = new Board();
-let ship1 = new Ship(5, [[0, 1],
+
+let ship1 = new Ship(5, [
+  [0, 1],
   [0, 2],
   [0, 3],
   [0, 4],
-  [0, 5],]);
-let ship2 = new Ship(3);
-let ship3 = new Ship(3);
-let ship4 = new Ship(2);
-let ship5 = new Ship(1);
+  [0, 5],
+]);
 
+// Fixing ship creation by providing coordinates
+let ship2 = new Ship(3, [
+  [2, 1],
+  [2, 2],
+  [2, 3],
+]);
 
+let ship3 = new Ship(3, [
+  [4, 4],
+  [4, 5],
+  [4, 6],
+]);
 
+let ship4 = new Ship(2, [
+  [6, 7],
+  [6, 8],
+]);
 
+let ship5 = new Ship(1, [[8, 9]]);
+
+// Function to check if a coordinate hits ship1
 function checking(input) {
-  return ship1.match(input);
+  if (ship1.match(input)) {
+    ship1.hit();
+    console.log("Hit confirmed!");
+    return true;
+  } else {
+ 
+    console.log("Miss!");
+    return false;
+  }
 }
-checking([0,1]);
+
+ // Should log "Ship hit!" and "Hit confirmed!"
+// Should log "Ship hit!" and "Hit confirmed!"
+checking([0,1])
+checking([0,1])
+checking([0,5])
+checking([0,5])
 
 
