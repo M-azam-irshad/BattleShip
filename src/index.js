a95066962397635125b22c7d529a4fa4331fa168
M-azam-irshad <196023029+M-azam-irshad@users.noreply.github.com>
2025-02-16 10:25:36 +0500

    Assigned coordinates, removed erros, made it work: All the children of "grid" element were assignd their respective coordinates. Chatgpt removed errors and modified code, such as if-else were replaced by simple for-of. Player board is working perfectly

--- src/index.js
+++ src/index.js
@@ -24,31 +24,21 @@ class Ship {
     return this.#hits >= this.#length;
   }
 
-  match(input)
-  {
-    if (this.#coords.some(([x, y]) => x === input[0] && y === input[1]))
-      {
-        usedCell.push(input)
-        console.log(usedCell);
-        
-        return true;
-      }
-      else{
-        return false;
-      }
+  match(input) {
+    if (this.#coords.some(([x, y]) => x === input[0] && y === input[1])) {
+      usedCell.push(input);
+      console.log(usedCell);
+      return true;
+    }
+    return false;
   }
+
   validation(input) {
-    
-    if(usedCell.some(([x,y]) => x === input[0] && y === input[1]))
-    {
-      console.log("Already exist");
-      
+    if (usedCell.some(([x, y]) => x === input[0] && y === input[1])) {
+      console.log("Already attacked!");
       return false;
     }
-    return this.match(input)
-
-
-
+    return this.match(input);
   }
 }
 
@@ -72,83 +62,90 @@ class Board {
   }
 }
 
+// Initialize Boards
 let B1 = new Board();
 let B2 = new Board();
 
-let ship1 = new Ship(5, [
-  [0, 1],
-  [0, 2],
-  [0, 3],
-  [0, 4],
-  [0, 5],
-]);
+// Create Ships
+let ships = [
+  new Ship(5, [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5]]),
+  new Ship(3, [[2, 1], [2, 2], [2, 3]]),
+  new Ship(3, [[4, 4], [4, 5], [4, 6]]),
+  new Ship(2, [[6, 7], [6, 8]]),
+  new Ship(1, [[8, 9]])
+];
 
-// Fixing ship creation by providing coordinates
-let ship2 = new Ship(3, [
-  [2, 1],
-  [2, 2],
-  [2, 3],
-]);
-
-let ship3 = new Ship(3, [
-  [4, 4],
-  [4, 5],
-  [4, 6],
-]);
-
-let ship4 = new Ship(2, [
-  [6, 7],
-  [6, 8],
-]);
-
-let ship5 = new Ship(1, [[8, 9]]);
-
-// Function to check if a coordinate hits ship1
+// Function to check if a coordinate hits any ship
 function checking(input) {
-  if (ship1.validation(input)) {
-    ship1.hit();
-    console.log("Hit confirmed!");
-    return true;
-  } 
-  else if(ship2.validation(input))
-  {
-    ship2.hit();
-    console.log("Ship2 hit");
-    return true;
+  for (let ship of ships) {
+    if (ship.validation(input)) {
+      ship.hit();
+      console.log("Hit confirmed!");
+      return true;
+    }
   }
-  else if(ship3.validation(input))
-  {
-    ship3.hit();
-    console.log("Ship3 hit");
-    return true;
-  }
-  else if(ship4.validation(input))
-  {
-    ship4.hit();
-    console.log("Ship4 hit");
-    return true;
-  }
-  else if(ship5.validation(input))
-  {
-    ship5.hit();
-    console.log("Ship5 hit");
-    return true;
-  }
-
-  else {
- 
-    console.log("Miss!");
-    return false;
-  }
+  console.log("Miss!");
+  return false;
 }
 
- // Should log "Ship hit!" and "Hit confirmed!"
-// Should log "Ship hit!" and "Hit confirmed!"
+// Create Grids in the DOM
+let grid1 = document.getElementById("grid");
+let grid2 = document.getElementById("grid2");
 
-checking([0,1]);
-checking([2,3]);
-checking([2,3]);
+// function createGrid(gridElement, board) {
+//   let fragment = document.createDocumentFragment();
+//   for (let i = 0; i < 10; i++) {
+//     for (let j = 0; j < 10; j++) {
+//       let cell = document.createElement("div");
+//       cell.className = "grid-child";
+//       cell.dataset.coord = JSON.stringify([i, j]); // Store coordinates
+//       cell.addEventListener("click", () => handleCellClick([i, j], cell));
+//       fragment.appendChild(cell);
+//     }
+//   }
+//   gridElement.appendChild(fragment);
+// }
 
-// checking(); 
+// // Handle Click on Grid Cells
+// function handleCellClick(coord, cell) {
+//   if (checking(coord)) {
+//     cell.style.backgroundColor = "red"; // Mark hit
+//   } else {
+//     cell.style.backgroundColor = "gray"; // Mark miss
+//   }
+// }
 
+// Create Both Player and Computer Grids
 
+function createGrid(gridElement,board)
+{
+  let fragment = document.createDocumentFragment();
+  for(let i=0; i < 10; i++)
+  {
+    for(let j=0; j< 10; j++)
+    {
+      let cell = document.createElement("div");
+      cell.className = "grid-child";
+      cell.dataset.coord = JSON.stringify([i,j]);
+      cell.addEventListener("click", ()=>handleCellClick([i,j],cell)) 
+      fragment.appendChild(cell)
+    }
+  }
+  gridElement.appendChild(fragment);
+}
+
+function handleCellClick(coord,cell)
+{
+  if(checking(coord))
+  {
+    cell.style.background = "red";
+  }
+  else
+  {
+    cell.style.background = "grey";
+  }
+}
+createGrid(grid1, B1);
+createGrid(grid2, B2);
+
+console.log("Game Initialized!");
